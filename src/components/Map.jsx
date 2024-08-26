1b45061c947ca509fc72da70209b9c71bb4e1a18
Diogo Coelho <diogocoelho19988@gmail.com>
2024-08-26 11:43:19 +0100

    geolocation added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,23 +3,33 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import { MapContainer,TileLayer, Marker, Popup, useMap, useMapEvent } from "react-leaflet";
 import { useState, useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext.jsx";
+import { useGeolocation } from "../hooks/useGeolocation.js";
+
+import Button from "../components/Button.jsx";
 
 export default function Map() {
     const [searchParams] = useSearchParams();
+    const [mapPosition, setMapPosition] = useState([39.97899046830462, -3.7475582957267766]);
+
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
 
-    const [mapPosition, setMapPosition] = useState([38.7, -9.2]);
     const {cities} = useCities();
+    const {isLoading: loadingPosition, position: geoPosition, getPosition} = useGeolocation();
     
     // Store the values of lat/lng from the last city viewed -> Fix point when the user moves from city to city on the map 
     useEffect(() => {
         if (lat && lng) setMapPosition([lat, lng]);
     }, [lat, lng])
+
+    useEffect(() => {
+        if (geoPosition) setMapPosition([geoPosition.lat, geoPosition.lng]);
+    }, [geoPosition])
     
     return(
         <div className={styles.mapContainer}>
-            <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
+            <Button type="position" onClick={getPosition}>{loadingPosition ? "Loading ..." : "Get Location"}</Button>
+            <MapContainer className={styles.map} center={mapPosition} zoom={6} scrollWheelZoom={true}>
             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
             {cities.map((city, i) => 
                 <Marker key={i} position={[city.position.lat, city.position.lng]}>
@@ -33,7 +43,7 @@ export default function Map() {
     )
 }
 
-// Center the map dynamically
+// Centers the map dynamically
 function ChangeCenter({position}) {
     const map = useMap();
     map.setView(position);
